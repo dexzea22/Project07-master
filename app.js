0819519a1a88441803e9f9ccba5b3db601dd9175
dexzea22 <77290837+dexzea22@users.noreply.github.com>
2023-07-11 22:52:37 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+var createError = require('http-errors');
+var express = require('express');
+const session = require('express-session');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var loginRouter = require('./routes/index');
+var registerRouter = require('./routes/register');
+var studentInfoRouter = require('./routes/studentinfo');
+var userinfoRouter = require('./routes/userinfo');
+var app = express();
+
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 86400000 // 24 hours
+  }
+}));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', loginRouter);
+app.use('/', registerRouter);
+app.use('/', studentInfoRouter);
+app.use('/', userinfoRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+
+  if (req.session.userId) {
+    return res.redirect('/');
+  }
+
+  res.render('error');
+});
+
+module.exports = app;
